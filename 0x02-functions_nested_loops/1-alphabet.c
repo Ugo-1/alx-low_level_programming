a6eeb2e8902fd9c3756b903dc3381626b0333ba5
Ugo-1 <uumeh428@gmail.com>
2022-03-15 22:42:56 -0700

    Commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print-alphabet - to print the alphabet
+ */
+
+void print_alphabet(void)
+{
+  char ch;
+
+  ch = 'a';
+
+  while (ch <= 'z')
+    {
+      _putchar(ch);
+      ch++;
+    }
+
+  _putchar('\n')
+
+}
