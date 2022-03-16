bf84cae180858a640e8da6126dbc84bf96b04ea8
Ugochukwu <uumeh428@gmail.com>
2022-03-16 06:44:12 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,16 +7,16 @@
 
 void print_alphabet(void)
 {
-  char ch;
+        char ch;
 
-  ch = 'a';
+	ch = 'a';
 
-  while (ch <= 'z')
-    {
-      _putchar(ch);
-      ch++;
-    }
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+      		ch++;
+    	}
 
-  _putchar('\n')
+  	_putchar('\n')
 
 }
